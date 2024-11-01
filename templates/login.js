d84160eb0e43b982547476989f7f2702c02a080b
Elli Lehtimäki <elli.i.lehtimaki@gmail.com>
2024-11-01 09:28:31 +0200

    FORGOT PASSWORD? goes to an other page

---
+++ templates/login.js
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+// Halutaanko zoomaus ominaisuus vai ei?
+// document.addEventListener('wheel', function(event) {
+//     if (event.ctrlKey) {
+//         event.preventDefault();
+//     }
+// }, { passive: false });
+
+// document.addEventListener('touchmove', function(event) {
+//     if (event.scale !== 1) {
+//         event.preventDefault();
+//     }
+// }, { passive: false });
