1f198db9d82d2ed1c57dff2d2fc04a9f90b9f2a9
Valma Haavisto <valma.haavisto@tuni.fi>
2024-11-28 17:55:03 +0200

    Added gps field for adding a shop

--- routes/shops.py
+++ routes/shops.py
@@ -1,5 +1,6 @@
 from flask import request, redirect, url_for, flash
 from models import db, Product, Shop, Price, User, WorksFor
+from database_functions import create_shop, add_user, add_shopkeeper_to_shop
 from datetime import datetime
 import random
 
@@ -9,7 +10,8 @@ def add_shop():
     shop_name = request.form.get('store_name')
     shop_chain = request.form.get('chain')
     shop_location = request.form.get('location_address')
-    shopkeepers = request.form.get('shopkeepers')  
+    shopkeepers = request.form.get('shopkeepers') 
+    gps = request.form.get('gps') 
 
     if not shop_name or not shop_chain or not shop_location:
         flash("Please provide all required shop details.", "shop")
@@ -17,41 +19,48 @@ def add_shop():
 
     try:
         # Set creation_date to now
-        creation_date = datetime.now().date()  # Get the current date
+        #creation_date = datetime.now().date()  # Get the current date
 
-        new_shop = Shop(
-            shop_id = shop_id,
-            store_name=shop_name,
-            store_chain=shop_chain,
-            location_address=shop_location,
-            user_created=1,  # Adjust as needed
-            creation_date=creation_date
-        )
+        #new_shop = Shop(
+            #shop_id = shop_id,
+           # store_name=shop_name,
+            #store_chain=shop_chain,
+            #location_address=shop_location,
+            #user_created=1,  # Adjust as needed
+            #creation_date=creation_date
+        #)
+        create_shop(shop_name, shop_chain, shop_location, gps)
 
-        db.session.add(new_shop)
-        db.session.commit()  # Commit to get the new shop ID
+        #db.session.add(new_shop)
+        #db.session.commit()  # Commit to get the new shop ID
 
         if shopkeepers:
             shopkeeper_names = [name.strip() for name in shopkeepers.split(',')]
             for shopkeeper_name in shopkeeper_names:
+                add_user(shopkeeper_name, password="default", email="default2", role="shopkeeper")
+                #TODO: change this to search with user id, not name
                 shopkeeper = User.query.filter_by(username=shopkeeper_name).first()
+                add_shopkeeper_to_shop(shopkeeper.user_id, shop_id)
+
+
+                #shopkeeper = User.query.filter_by(username=shopkeeper_name).first()
                 
-                if not shopkeeper:
+                #if not shopkeeper:
                     # Create new shopkeeper if not found
-                    shopkeeper = User(
-                        username=shopkeeper_name,
-                        password='default',  # Set a secure password as needed
-                        role='shopkeeper',
-                        aura_points=0
-                    )
-                    db.session.add(shopkeeper)  # Add the new shopkeeper to the db.session
-                    db.session.commit()  # Commit here to flush the new user ID
+                    #shopkeeper = User(
+                        #username=shopkeeper_name,
+                        #password='default',  # Set a secure password as needed
+                        #role='shopkeeper',
+                        #aura_points=0
+                    #)
+                    #db.session.add(shopkeeper)  # Add the new shopkeeper to the db.session
+                    #db.session.commit()  # Commit here to flush the new user ID
 
                 # Now add the relationship
-                works_for = WorksFor(user_id=shopkeeper.user_id, shop_id=new_shop.shop_id)
-                db.session.add(works_for)
+                #works_for = WorksFor(user_id=shopkeeper.user_id, shop_id=new_shop.shop_id)
+                #db.session.add(works_for)
         
-        db.session.commit()
+        #db.session.commit()
         print("Shop added successfully!", "shop")
     except (ValueError, IndexError):
         print("Error processing the provided details. Please ensure all fields are filled out correctly.", "shop")
