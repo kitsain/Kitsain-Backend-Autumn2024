764f82c9f008bf32ac092e564261d167bca93743
ehkuitti <72074501+ehkuitti@users.noreply.github.com>
2024-12-09 23:24:50 +0200

    Remove old commented-out code

--- templates/login.js
+++ templates/login.js
@@ -31,26 +31,3 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
 
 });
 
-// function togglePasswordVisibility() {
-//     const passwordInput = document.getElementById("userpassword");
-//     const toggleButton = document.getElementById("togglePassword");
-//     if (passwordInput.type === "password") {
-//         passwordInput.type = "text"; 
-//         toggleButton.innerHTML = "&#128275;"; 
-//     } else {
-//         passwordInput.type = "password"; 
-//         toggleButton.innerHTML = "&#128274;";
-//     }
-// }
-// Halutaanko zoomaus ominaisuus vai ei?
-// document.addEventListener('wheel', function(event) {
-//     if (event.ctrlKey) {
-//         event.preventDefault();
-//     }
-// }, { passive: false });
-
-// document.addEventListener('touchmove', function(event) {
-//     if (event.scale !== 1) {
-//         event.preventDefault();
-//     }
-// }, { passive: false });
